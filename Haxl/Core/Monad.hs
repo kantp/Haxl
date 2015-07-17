96c77db755e180c3318ec07e6772266f2e82b24a
Philipp Kant <philipp@infinipool.com>
2015-07-17 14:57:11 +0200

    Close thunk leak in roundstats.
    The function performFetches collects statistics about the requests
    performed in each round. However, the list is not evaluated (unless
    tracing is on), leading to an accumulation of thunks. Forcing the list
    via deepseq prevents this.

--- Haxl/Core/Monad.hs
+++ Haxl/Core/Monad.hs
@@ -57,6 +57,7 @@ import Control.Exception (AllocationLimitExceeded(..))
 import Control.Monad
 import qualified Control.Exception as Exception
 import Control.Applicative hiding (Const)
+import Control.DeepSeq
 import GHC.Exts (IsString(..))
 #if __GLASGOW_HASKELL__ < 706
 import Prelude hiding (catch)
@@ -589,7 +590,7 @@ performFetches n env reqs = do
   let roundtime = realToFrac (diffUTCTime t1 t0) :: Double
 
   ifReport f 1 $
-    modifyIORef' sref $ \(Stats rounds) ->
+    modifyIORef' sref $ \(Stats rounds) -> roundstats `deepseq`
       Stats (RoundStats (microsecs roundtime) dsroundstats: rounds)
 
   ifTrace f 1 $
